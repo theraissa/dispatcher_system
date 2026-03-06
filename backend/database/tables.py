5ac8ed62d41583e54f2c166153cb74716ca91996
Raissa Maciel Lima <macielraissalima@gmail.com>
2026-03-05 22:40:42 -0300

    novas tabelas no banco

--- backend/database/tables.py
+++ backend/database/tables.py
@@ -4,43 +4,58 @@ Módulo com as tabelas do banco de dados
 
 from database import db
 from sqlalchemy import Column, DateTime, Integer
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.sql import func
 
 
-class UserClient(db.Model): 
+class UserDB(db.Model): 
     """Representa um cliente"""
 
-    __tablename__ = "user_client"
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    cpf_cliente = Column(String, nullable=False)
-    rg_cliente = Column(String, nullable=False)
-    nome_cliente = Column(String, nullable=False)
-    nasc_cliente = Column(String)
-    tele_pessoal_cliente = Column(String)
-    email_cliente = Column(String, nullable=False)
-    senha_cliente = Column(String, nullable=False)
+    cpf = Column(String) 
+    rg = Column(String) 
+    name = Column(String) 
+    date_birth = Column(String)
+    contact = Column(String)
+    email = Column(String) 
+    password = Column(String) 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
     deleted_at = Column(DateTime(timezone=True))
 
 
-class UserDispatcher(db.Model): 
+class DispatcherDB(db.Model): 
     """Representa um despachante"""
 
-    __tablename__ = "user_dispatcher"
+    __tablename__ = "dispatcher"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    cpf_desp = Column(String, nullable=False)
-    rg_desp = Column(String, nullable=False)
-    nome_desp = Column(String, nullable=False)
-    nasc_desp = Column(String)
-    tele_pessoal_desp = Column(String)
-    email_desp = Column(String, nullable=False)
-    senha_desp = Column(String, nullable=False)
-    regis_crdd = Column(String, nullable=False)
-    data_exp_regis = Column(DateTime, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    regis_crdd = Column(String)
+    date_exp_regis = Column(DateTime)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    deleted_at = Column(DateTime(timezone=True))
+
+
+class OfficeDB(db.Model): 
+    """Representa um despachante"""
+
+    __tablename__ = "office"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    dispatcher_id = Column(Integer, ForeignKey("dispatcher.id"), nullable=False)
+    regis_crdd = Column(String)
+    data_exp_regis = Column(DateTime)
+    contact = Column(String)
+    number = Column(Integer) 
+    neighborhood = Column(String)
+    address = Column(String)
+    city = Column(String)
+    state = Column(String)
+    zip_code = Column(Integer)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
     deleted_at = Column(DateTime(timezone=True))
--- backend/models/user_client.py
+++ backend/models/user_client.py
@@ -7,38 +7,38 @@ from datetime import datetime
 from pydantic import BaseModel, ConfigDict, RootModel
 
 
-class CreateUserClientRequest(BaseModel):
+class CreateUserRequest(BaseModel):
     """Modelo de criação do serviço User Client."""
 
-    cpf_cliente: Optional[str] = None
-    rg_cliente: Optional[str] = None
-    nome_cliente: Optional[str] = None
-    nasc_cliente: Optional[str] = None
-    tele_pessoal_cliente: Optional[str] = None
-    email_cliente: Optional[str] = None
-    senha_cliente: Optional[str] = None
+    cpf : Optional[str] = None
+    rg : Optional[str] = None
+    name : Optional[str] = None
+    date_birth: Optional[str] = None
+    contact: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
     
     model_config = ConfigDict(extra="forbid")
 
 
-class UserClientResponse(BaseModel):
+class UserResponse(BaseModel):
     """Modelo de resposta para o serviço User Client."""
 
     id: int
-    cpf_cliente: Optional[str]
-    rg_cliente: Optional[str]
-    nome_cliente: Optional[str]
-    nasc_cliente: Optional[str]
-    tele_pessoal_cliente: Optional[str]
-    email_cliente: Optional[str]
-    senha_cliente: Optional[str]
+    cpf : Optional[str]
+    rg : Optional[str]
+    name : Optional[str]
+    date_birth: Optional[str]
+    contact: Optional[str]
+    email: Optional[str]
+    password: Optional[str]
     created_at: datetime
     updated_at: datetime
     deleted_at: Optional[datetime]
-
+    
     model_config = ConfigDict(from_attributes=True)
 
 class ListUserClientResponse(RootModel):
     """Modelo de listagem para o serviço User Client"""
 
-    root: List[UserClientResponse]
+    root: List[UserResponse]
